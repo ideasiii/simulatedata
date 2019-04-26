4d7795bd9b235e2b10b33d1d7f9d340a08ab69a6
蔡育承 <ntycifc0413@gmail.com>
2019-04-26 12:11:09 +0800

    更新pareto/poisson distribution之執行範例

--- src/org/iii/simulatedata/utils/probabilityDistribution/ParetoDistFuncs.java
+++ src/org/iii/simulatedata/utils/probabilityDistribution/ParetoDistFuncs.java
@@ -2,6 +2,7 @@ package org.iii.simulatedata.utils.probabilityDistribution;
 
 import org.apache.commons.math3.distribution.ParetoDistribution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ParetoDistFuncs extends ParetoDistribution {
@@ -11,5 +12,11 @@ public class ParetoDistFuncs extends ParetoDistribution {
     }
 //    public static void main(String[] args){
 //        System.out.println(Arrays.toString(ParetoDistFuncs(20)));//pareto dist
+//        double[] resultValue = ParetoDistFuncs(200);
+//        ArrayList tmpResult = new ArrayList();
+//        for (int i = 0; i < resultValue.length; i++){
+//            tmpResult.add(Math.exp(resultValue[i]) * 100000);
+//        }
+//        System.out.println(tmpResult);
 //    }
 }
--- src/org/iii/simulatedata/utils/probabilityDistribution/PoissonDistFuncs.java
+++ src/org/iii/simulatedata/utils/probabilityDistribution/PoissonDistFuncs.java
@@ -3,6 +3,7 @@ package org.iii.simulatedata.utils.probabilityDistribution;
 import org.apache.commons.math3.distribution.PoissonDistribution;
 import org.apache.commons.math3.exception.NotStrictlyPositiveException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PoissonDistFuncs extends PoissonDistribution {
@@ -16,6 +17,17 @@ public class PoissonDistFuncs extends PoissonDistribution {
         return poissonRG.sample(r);
     }
 //    public static void main(String[] args){
-//        System.out.println(Arrays.toString(PoissonDistFuncs(20,1)));//poisson dist
+//        System.out.println(Arrays.toString(PoissonDistFuncs(200,1)));//poisson dist
+//
+//        /*
+//        同時伸縮
+//         */
+//        int[] testResult = PoissonDistFuncs(200,3);
+//        ArrayList testResultRev = new ArrayList();
+//        for (int i=0; i < testResult.length; i++){
+//            double resultRev = (double) testResult[i] / 10;
+//            testResultRev.add(resultRev);
+//        }
+//        System.out.println(testResultRev);
 //    }
 }
